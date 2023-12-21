68de75ca17d346e33a25e911bbf8de31f8b5a08f
JKomosa <j.komosa.347@studms.ug.edu.pl>
2023-12-21 12:59:40 +0100

    add debounce

--- malazan_names.js
+++ malazan_names.js
@@ -1,3 +1,4 @@
+/*navbar dropdown*/
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -7,25 +8,39 @@ function myFunction() {
   }
 }
 
-function debounce(func, ms) {
-  let timeoutId;
+/*basic JavaScript debounce function as taken from Underscore.js*/
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+			console.log( "Handler for `debounce` called." );
 
-  return function newFunc(...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(func, ms, ...args);
-  }
-}
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}; 
 
-$('#search').keyup(function() {
+/*filter search for multiple parameters, taken from somewhere on stackoverflow*/
+$('#search').on( "keyup",debounce( function(){
+	  	console.log( "Start after debouncing."	);
 
   var $rows = $('#table tr');
-  var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-    reg = RegExp(val, 'i'),
+  var Values = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+    reg = RegExp(Values, 'i'),
     text;
+	  	console.log( "Regular expression created."	);
 
   $rows.show().filter(function() {
     text = $(this).text().replace(/\s+/g, ' ');
     return !reg.test(text);
   }).hide();
+  	  	console.log( "Search filter."	);
 
-});
+
+},800));
